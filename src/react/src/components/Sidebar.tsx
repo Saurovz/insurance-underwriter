502a434ee5a134c9e52388aab2050a0ec62c3e85
Shashidhara Naik <shnaik@teksystems.com>
2026-01-30 13:59:11 +0530

    Added home page with tiles

--- src/react/src/components/Sidebar.tsx
+++ src/react/src/components/Sidebar.tsx
@@ -4,7 +4,10 @@ import { NavLink } from "react-router-dom";
 export const Sidebar: React.FC = () => {
   return (
     <div className="sidebar">
-      <h3 className="logo">🏠 HomePage</h3>
+      <h3 className="logo">
+        <NavLink to="/homepage">🏠 Home</NavLink>
+      </h3>
+
 
       <nav>
         <NavLink to="/application">📄 Application</NavLink>
